bf273b134212c34c6545644c02dd73f0940d210a
Saurabh Sikchi <saurabhrsikchi@gmail.com>
2015-11-28 19:45:33 +0530

    remove admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    DashboardManifest::DASHBOARDS.each do |dashboard_resource|
-      resources dashboard_resource
-    end
+  # namespace :admin do
+  #   DashboardManifest::DASHBOARDS.each do |dashboard_resource|
+  #     resources dashboard_resource
+  #   end
 
-    root controller: DashboardManifest::ROOT_DASHBOARD, action: :index
-  end
+  #   root controller: DashboardManifest::ROOT_DASHBOARD, action: :index
+  # end
 
   root 'user#new'
 
